1b0b8e9cbdcfcc0f7eca5cea34f280874ffc5266
Lumenwright <36579367+Lumenwright@users.noreply.github.com>
2018-03-22 16:13:24 -0600

    updated video sphere shader to be based on provided unlit shader
    Unfortunately video still doesn't seem to show on my Samsung S6. It's definitely playing because the signal at the end of the video triggers.

--- Main/Assets/Scripts/ChooseBranch.cs
+++ Main/Assets/Scripts/ChooseBranch.cs
@@ -12,6 +12,11 @@ public class ChooseBranch : MonoBehaviour {
 	public ParticleSystem snow;
 	public ParticleSystem rain;
 
+	public string whichBranch = "main";
+
+	public bool paused = false;
+	public bool waiting = true; // waiting for particle to appear at right time
+
 	float mainWaitTime = 20f;
 	float cryWaitTime = 5f;
 	float howlWaitTime = 5f;
@@ -33,18 +38,40 @@ public class ChooseBranch : MonoBehaviour {
 
 	// when sphere is clicked, will set true for cry branch and false for howl
 	// and set timing for particles of the branch to appear
-	public void MakeParticleAppear (bool chooseBranch){
-		if (chooseBranch) {
+	public void MakeParticleAppear (string chooseBranch){
+		if (chooseBranch == "cry") {
+			Debug.Log ("Starting particle coroutine");
 			StartCoroutine (ParticleAppear (cryWaitTime, rain));
 		}
 
-		if (chooseBranch == false) {
+		if (chooseBranch == "howl") {
+			Debug.Log ("Starting particle coroutine");
 			StartCoroutine (ParticleAppear (howlWaitTime, snow));
 		}
+
+		whichBranch = chooseBranch; // for play function to work after pause
 	}
 
 	IEnumerator ParticleAppear (float when, ParticleSystem ps){
-		yield return new WaitForSecondsRealtime(when);
+		waiting = true;
+
+		// wait additional time if video was paused before particles appeared
+		float timepassed = 0;
+		while (paused) {
+			timepassed = timepassed + 0.2f;
+			yield return new WaitForSeconds(0.2f); // wait a short length of time and then check if unpaused
+		}
+
+		yield return new WaitForSecondsRealtime (when);
+
+		Debug.Log ("Time while paused: "+timepassed);
+		yield return new WaitForSeconds (timepassed);
+
+		waiting = false;
 		ps.Play ();
+	}
+
+	public void Paused(){
+		paused = true;
 	}
 }
